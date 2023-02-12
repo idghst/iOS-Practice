80edec0ac0d3f033041d3fd78822a454ef942328
idghst <idghst.dev@gmail.com>
2023-02-13 02:42:41 +0900

    GithubUserProfile - MVVM 리팩토링 - 스토리보드

--- StoryBoard/GithubUserProfile/GithubUserProfile/View/SearchViewController.swift
+++ StoryBoard/GithubUserProfile/GithubUserProfile/View/SearchViewController.swift
@@ -10,32 +10,34 @@ import Combine
 import Kingfisher
 
 class UserProfileViewController: UIViewController {
-    
-    let network = NetworkService(configuration: .default)
-    
-    @Published private(set) var user: UserProfile?
+
     var subscription = Set<AnyCancellable>()
-    
+    var viewModel: SearchViewModel!
+
     @IBOutlet weak var thumbnail: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var loginLabel: UILabel!
     @IBOutlet weak var followerLabel: UILabel!
     @IBOutlet weak var followingLabel: UILabel!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewModel = SearchViewModel(
+            network: NetworkService(configuration: .default),
+            selectedUser: nil
+        )
         setupUI()
         embedSearchControl()
         bind()
     }
-    
+
     private func setupUI() {
         thumbnail.layer.cornerRadius = 80
     }
-    
+
     private func embedSearchControl() {
         self.navigationItem.title = "Search"
-        
+
         let searchController = UISearchController(searchResultsController: nil)
         searchController.hidesNavigationBarDuringPresentation = false
         searchController.searchBar.placeholder = "idghst"
@@ -43,31 +45,17 @@ class UserProfileViewController: UIViewController {
         searchController.searchBar.delegate = self
         self.navigationItem.searchController = searchController
     }
-    
+
     private func bind() {
-        $user
-            .print("debug")
+        viewModel.selectedUser
             .receive(on: RunLoop.main)
             .sink { [unowned self] user in
-                self.update(user)
-            }
-            .store(in: &subscription)
-    }
-    
-    private func update(_ user: UserProfile?) {
-        guard let user = user else {
-            self.nameLabel.text = "N/A"
-            self.loginLabel.text = "N/A"
-            self.followerLabel.text = ""
-            self.followingLabel.text = ""
-            self.thumbnail.image = nil
-            return
-        }
-        self.nameLabel.text = user.name
-        self.loginLabel.text = user.login
-        self.followerLabel.text = "follower : \(user.followers)"
-        self.followingLabel.text = "following : \(user.following)"
-        self.thumbnail.kf.setImage(with: user.avatarUrl)
+                self.nameLabel.text = viewModel.name
+                self.loginLabel.text = viewModel.login
+                self.followerLabel.text = viewModel.followers
+                self.followingLabel.text = viewModel.following
+                self.thumbnail.kf.setImage(with: self.viewModel.imageURL)
+            }.store(in: &subscription)
     }
 }
 
@@ -81,27 +69,9 @@ extension UserProfileViewController: UISearchResultsUpdating {
 extension UserProfileViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         print("button clicked : \(searchBar.text!)")
-        
+
         guard let keyword = searchBar.text, !keyword.isEmpty else { return }
-        
-        let resource = Resource<UserProfile>(
-            base: "https://api.github.com/",
-            path: "users/\(keyword)",
-            params: [:],
-            header: ["Content-Type": "application/json"]
-        )
-        
-        network.load(resource)
-            .receive(on: RunLoop.main)
-            .sink { completion in
-                switch completion {
-                case .failure(let error):
-                    self.user = nil
-                case .finished: break
-                }
-            } receiveValue: { result in
-                self.user = result
-            }
-            .store(in: &subscription)
+        viewModel.search(keyword: keyword)
+
     }
 }
