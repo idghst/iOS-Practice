8efad6847a87b81cf8a1a123916e03f05c740b38
idghst <idghst.dev@gmail.com>
2023-02-20 06:37:27 +0900

    DataFlow - SwiftUI

---
+++ SwiftUI/DataFlow-SwiftUI/DataFlow-SwiftUI/PlayingStatusView.swift
@@ -0,0 +1,20 @@
+//
+//  PlayingStatusView.swift
+//  DataFlow-SwiftUI
+//
+//  Created by idghst on 2023/02/19.
+//
+
+import SwiftUI
+
+struct PlayingStatusView: View {
+    @Binding var isPlaying: Bool
+    
+    var body: some View {
+        Image(systemName: isPlaying ? "sun.max.fill" : "sun.max")
+            .resizable()
+            .renderingMode(.original)
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 80, height: 80)
+    }
+}
