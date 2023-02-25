c443b65008560f738e8c71eb773d604fab1db758
idghst <idghst.dev@gmail.com>
2023-02-25 22:25:41 +0900

    SymbolRoller - SwiftUI

---
+++ SwiftUI/SymbolRoller-SwiftUI/SymbolRoller-SwiftUI/SymbolRoller_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolRoller_SwiftUIApp.swift
+//  SymbolRoller-SwiftUI
+//
+//  Created by idghst on 2023/02/25.
+//
+
+import SwiftUI
+
+@main
+struct SymbolRoller_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
